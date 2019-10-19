1ab286ca1a22038dba4f105ed6ff01514eba592b
Harel Mazor <harel.mazor@gmail.com>
2019-10-19 23:06:12 +0300

    Resolves #1 - Provide interfaces, moved TypedStream to a separate file, added some comments.

---
+++ src/TypedStream.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OsmSharp.IO.API
+{
+    public class TypedStream
+    {
+        public Stream Stream;
+        public string FileName;
+        public System.Net.Http.Headers.MediaTypeHeaderValue ContentType;
+
+        internal static async Task<TypedStream> Create(HttpContent content)
+        {
+            return new TypedStream
+            {
+                FileName = content.Headers.ContentDisposition?.FileName.Trim('"'),
+                ContentType = content.Headers.ContentType,
+                Stream = await content.ReadAsStreamAsync()
+            };
+        }
+    }
+}
+
